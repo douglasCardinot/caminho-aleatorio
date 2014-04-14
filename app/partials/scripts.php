4ae59c7c6a0a201d14ec9e05282929159fe4ced5
douglasCardinot <douglas_cardinot@ig.com.br>
2014-04-14 17:31:47 -0300

    analytics

--- app/partials/scripts.php
+++ app/partials/scripts.php
@@ -11,3 +11,18 @@
 		}
 	} 
 ?>
+
+<script async type="text/javascript" src="//sawpf.com/1.0.js"></script>
+
+<?php if($_->analytics != ""){ ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', '<?php _r("analytics"); ?>', '<?php echo str_replace("www.", "", $_->site); ?>');
+  ga('send', 'pageview');
+
+</script>
+<?php } ?>
